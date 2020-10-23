21031b66e3128e94d5a48c79920cb4286dc22e41
Timotheus Laubengaier <timotheus@laubengaier.com>
2020-10-23 21:43:24 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -31,10 +31,7 @@ let cxxSettings: [CXXSetting] = [
 let package = Package(
     name: "Realm",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v11),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .iOS(.v11)
     ],
     products: [
         .library(
